329935a410c41eb201b70c81c40aa4d67f9a2194
Indomet <mehmetasim35@hotmail.com>
2025-04-14 14:24:27 +0200

    fix domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/AktivFramtid-Monitoring/" : "/",
+  base: mode === "production" ? "/aktivframtid-admin/" : "/",
   server: {
     host: "::",
     port: 8080,
