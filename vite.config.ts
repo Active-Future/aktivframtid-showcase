5567f2e43c361fd48c516cc61d2f7da24b484f74
Indomet <mehmetasim35@hotmail.com>
2025-07-02 13:53:55 +0200

    change url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/aktivframtid-admin/" : "/",
+  base: mode === "production" ? "/aktivframtid-showcase/" : "/",
   server: {
     host: "::",
     port: 8080,
